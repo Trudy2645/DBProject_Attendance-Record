00a192e84fc940e1c26fd822c2696557e2e7e553
Trudy2645 <kims0509@pusan.ac.kr>
2025-09-29 16:51:25 +0900

    test

---
+++ lib/test.dart
@@ -0,0 +1,20 @@
+// lib/math_test.dart
+int add(int a, int b) {
+  // 나중에 기능 구현
+  return 0;
+}
+
+int subtract(int a, int b) {
+  // 나중에 기능 구현
+  return 0;
+}
+
+int multiply(int a, int b) {
+  // 나중에 기능 구현
+  return 0;
+}
+
+double divide(int a, int b) {
+  // 나중에 기능 구현
+  return 0.0;
+}
