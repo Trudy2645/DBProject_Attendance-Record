4600daab177d123b546ca036af5cf5b2407e02a6
Trudy2645 <kims0509@pusan.ac.kr>
2025-10-13 23:46:23 +0900

    Feat: CRUD 성공

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
 }
 
 android {
+
     namespace = "com.example.db_attendance"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
